7e4e90e047f1becd66e558879b92bfb47b230f2c
ItaEdgarLopez <ita.edgar.lopez@gmail.com>
2022-07-05 19:12:19 -0500

    package json

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import './index.css';
-import { FaBeer } from 'react-icons/fa';	
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
